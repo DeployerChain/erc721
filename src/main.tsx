e311bf2bf9366bd634f476c60bcc817fd3ef72ce
DeployerChain <marianriverasabog@yahoo.com>
2024-10-22 23:32:29 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -12,25 +12,40 @@ import {
   relayerUrlConst,
   clientIdConst,
 } from "./consts/parameters";
-
+import { defineChain } from "thirdweb";
 const container = document.getElementById("root");
 const root = createRoot(container!);
 const urlParams = new URL(window.location.toString()).searchParams;
 
+const customChain = {
+  // Required information for connecting to the network
+  chainId: 33139, // Chain ID of the network
+  rpc: ["https://apechain.calderachain.xyz/http"], // Array of RPC URLs to use
+ 
+  // Information for adding the network to your wallet (how it will appear for first time users) === \\
+  // Information about the chain's native currency (i.e. the currency that is used to pay for gas)
+  nativeCurrency: {
+    decimals: 18,
+    name: "ApeChain",
+    symbol: "APE",
+  },
+  shortName: "ApeChain", // Display value shown in the wallet UI
+  slug: "ApeChain", // Display value shown in the wallet UI
+  testnet: false, // Boolean indicating whether the chain is a testnet or mainnet
+  chain: "ApeChain", // Name of the network
+  name: "ApeChain", // Name of the network
+};
 const relayerUrl = urlParams.get("relayUrl") || relayerUrlConst || "";
 const biconomyApiKey =
   urlParams.get("biconomyApiKey") || biconomyApiKeyConst || "";
 const biconomyApiId =
   urlParams.get("biconomyApiId") || biconomyApiIdConst || "";
 const sdkOptions = getGasless(relayerUrl, biconomyApiKey, biconomyApiId);
-
-const chain = (urlParams.get("chain") && urlParams.get("chain")?.startsWith("{")) ? JSON.parse(String(urlParams.get("chain"))) : urlParams.get("chain") || chainConst;
-
 const clientId = urlParams.get("clientId") || clientIdConst || "";
 
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={chain} sdkOptions={sdkOptions} clientId={clientId}>
+    <ThirdwebProvider activeChain={customChain}  sdkOptions={sdkOptions} clientId={clientId}>
       <Toaster />
       <App />
     </ThirdwebProvider>
